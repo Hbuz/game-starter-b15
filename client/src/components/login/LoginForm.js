0b7ca16d1edb72beefae57c2bdc01ba6133e2276
Pooja <pooja.kaul2015@gmail.com>
2018-09-20 18:24:34 +0200

    changes

--- client/src/components/login/LoginForm.js
+++ client/src/components/login/LoginForm.js
@@ -21,21 +21,21 @@ export default class LoginForm extends PureComponent {
 		return (
       <div className="login-form">
   			<form onSubmit={this.handleSubmit}>
-  				<label>
+  				<label className="font-style">
             Email
             <input type="email" name="email" value={
   						this.state.email || ''
   					} onChange={ this.handleChange } />
-          </label>
+          </label >
 
-  				<label>
+  				<label className="Font-style-pass">
             Password
             <input type="password" name="password" value={
   						this.state.password || ''
   					} onChange={ this.handleChange } />
           </label>
 
-  				<button type="submit">Login</button>
+  				<button className="Button-style" type="submit">Login</button>
   			</form>
 		  </div>)
 	}
